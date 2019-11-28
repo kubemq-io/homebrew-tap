8f33cd7fc949d04551f9ecdb0830f39b2af2d873
goreleaserbot <goreleaser@carlosbecker.com>
2019-11-28 15:37:36 +0200

    Brew formula update for kubemqctl version v2.1.1

--- kubemqctl.rb
+++ kubemqctl.rb
@@ -2,16 +2,16 @@
 class Kubemqctl < Formula
   desc ""
   homepage ""
-  version "2.1.0"
+  version "2.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/kubemq-io/kubemqctl/releases/download/v2.1.0/kubemqctl_2.1.0_darwin_amd64.tar.gz"
-    sha256 "87d84e221b98be79f546e381ed8d2a286a3855728f75b2a3514deaee0984b748"
+    url "https://github.com/kubemq-io/kubemqctl/releases/download/v2.1.1/kubemqctl_2.1.1_darwin_amd64.tar.gz"
+    sha256 "27ae76bdc37c984e04bf2ff470a0fc60f6883b25dfb82ab76754bea5c15c8b8d"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kubemq-io/kubemqctl/releases/download/v2.1.0/kubemqctl_2.1.0_linux_amd64.tar.gz"
-      sha256 "6ea24c09c19d8dba3fecee1f0874f9857dc661bc2267b50459a4235e6dd69115"
+      url "https://github.com/kubemq-io/kubemqctl/releases/download/v2.1.1/kubemqctl_2.1.1_linux_amd64.tar.gz"
+      sha256 "fc45f743669ba84b656230107eafba04aa2f3cdd23102b5fce956bcd60b1c3ad"
     end
   end
 
